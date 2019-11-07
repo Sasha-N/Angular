7c5a822acabd432943195bc70f412044fcebb1b9
Sasha-N <avgystina17@mail.ru>
2019-11-07 18:41:45 +0700

    I made Material D

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TableComponent } from './table/table.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatInputModule} from '@angular/material/input';
 
 
 @NgModule({
@@ -14,7 +18,10 @@ TableComponent
 
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+MatSliderModule,
+MatInputModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+export class InputOverviewExample {}
+
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css']
 })
+
 export class TableComponent implements OnInit {
 
   constructor() { }
